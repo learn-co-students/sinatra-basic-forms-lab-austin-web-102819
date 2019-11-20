74f6f97f689cb545282e73240ae98528d7a5ca18
rachfuerst <rach.fuerst@gmail.com>
2019-11-20 15:55:21 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get "/" do
+        erb :index 
+    end
+
+    get "/new" do
+        erb :create_puppy
+    end
+
+    post "/puppy" do
+        # We want to create a new puppy object based off of what is input in the form:
+        @puppy= Puppy.new(params[:name], params[:breed], params[:age]) 
+        erb :display_puppy
+    end
+
+    
 end
